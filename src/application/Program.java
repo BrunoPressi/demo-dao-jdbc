d905dedc0cc06992b31da85bca6145847be9f2e8
Bruno Pressi <brunopressi2012@gmail.com>
2025-03-14 13:03:11 -0300

    feat: insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -23,12 +24,14 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("\n===== TEST: 3 seller findAll =====");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
+		System.out.println("\n===== TEST: 3 seller insert =====");
+		sellerDao.insert(new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.0, department));
 	}
 }
