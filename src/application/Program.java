8dfc36060498431a7b7cae9ee1cd50f5e8fb9c4c
Bruno Pressi <brunopressi2012@gmail.com>
2025-03-14 09:38:20 -0300

    feat: added seller and department classes

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import model.entities.Seller;
+
+public class Program {
+	public static void main(String[] args) {
+		/*Department department = new Department(1, "Book");
+		System.out.println(department);*/		
+	}
+}
